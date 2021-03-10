c6d09268b8ddf27332f80a12ceb005decfb16897
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-03-10 16:21:34 -0500

    Background gif is working, logo and pokebola

--- src/main.js
+++ src/main.js
@@ -3,4 +3,24 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let section1 = document.getElementById('section1');
+let section2 = document.getElementById('section2');
+let section3 = document.getElementById('section3');
+
+document.getElementById('pokebola1').addEventListener('click',ContinueSection2);
+
+function Ocultar(){
+    section1.style.display = 'none'
+    section2.style.display = 'none'
+    section3.style.display = 'none'
+}
+
+function ContinueSection2(){
+    Ocultar()
+    section2.style.display = 'block'
+}
+
+function ContinueSection3(){
+    Ocultar()
+    section3.style.display = 'block'
+}
