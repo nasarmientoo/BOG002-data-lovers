1e19785ba4ae5dc15fa713da9fd36fc833b963a8
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-03-18 20:09:26 -0500

    Background e iconos estáticos, primera aproximación función de simbología

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import data from './data/pokemon/pokemon.js';
 
 let section1 = document.getElementById('section1');
 let section2 = document.getElementById('section2');
-let container = document.getElementById('pocket_Container')
+let container = document.getElementById('pocket_Container');
 
 document.getElementById('pokebola1').addEventListener('click',() => Continue(section2));
+document.getElementById('icon-pokebola').addEventListener('click',() => Continue(section1));
+
 
 function Ocultar(){
     section1.style.display = 'none'
@@ -17,12 +19,20 @@ function Continue(section){
     section.style.display = 'block'
 }
 
+/*function rarityIcon(data){
+    for (let i = 0; i < data.pokemon.length; i++) {
+        if (data.pokemon[i][pokemon-rarity] === "normal"){
+           return "images/icons/pockemon-52.png"
+        }
+    }
+}*/
+
 function appendData(data){
     for (let i = 0; i < data.pokemon.length; i++) {     
         container.innerHTML += 
             `<div class="card">
                 <div class= "border-small">
-                    <h2 class="poke-Number">N.° ${data.pokemon[i].num} </h2>
+                    <h2 class="poke-Number">N.° ${data.pokemon[i].num} </h2>          
                     <img class="img-container" src=${data.pokemon[i].img}><br>
                     <p> ${data.pokemon[i].name} </p> 
                     <p class="description"> ${data.pokemon[i].about} </p>
@@ -30,6 +40,8 @@ function appendData(data){
             </div>`
     }
 }
+
+//<img class ="icon-rarity" src=${rarityIcon()}>
 
 fetch("./data/pokemon/pokemon.json")
         .then(function(response){
