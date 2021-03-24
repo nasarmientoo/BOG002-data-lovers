5e69ac9d3d1ffb1d403b7de0eb148447d0130b4f
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-03-24 09:15:41 -0500

    Top header e iconos estáticos

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ function appendData(data){
         container.innerHTML += 
             `<div class="card">
                 <div class= "border-small">
-                    <h2 class="poke-Number">N.° ${data.pokemon[i].num}</h2>          
+                    <h2 class="poke-Number">N.° ${data.pokemon[i].num} </h2>      
                     <img class="img-container" src=${data.pokemon[i].img}><br>
                     <p> ${data.pokemon[i].name} </p> 
                     <p class="description"> ${data.pokemon[i].about} </p>
