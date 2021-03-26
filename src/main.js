152b9aacc5f50946dfb592663d6bf20bc36c2327
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-03-25 19:36:41 -0500

    La primera aproximación de la ventana emergente falló

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import data from './data/pokemon/pokemon.js';
 let section1 = document.getElementById('section1');
 let section2 = document.getElementById('section2');
 let container = document.getElementById('pocket_Container');
+let cards = document.getElementsByClassName('card');
+let popup = document.getElementById('pop-up');
 
 document.getElementById('pokebola1').addEventListener('click',() => Continue(section2));
 document.getElementById('icon-pokebola').addEventListener('click',() => Continue(section1));
+cards.addEventListener('click',(e) => Show(popup,e));
+document.getElementById('close').addEventListener('click',() => Cover(popup))
 
 function Hide(){
     section1.style.display = 'none'
@@ -18,6 +22,15 @@ function Continue(section){
     section.style.display = 'block'
 }
 
+function Show(section,x){
+    console.log(x.target)
+    section.style.display = 'block'
+}
+
+function Cover(section){
+    section.style.display = 'none'
+}
+
 function Capital(x){
     return x[0].toUpperCase() + x.substring(1);
 }
@@ -26,7 +39,7 @@ function AppendData(data){
     let contenedor = ""
     for (let i = 0; i < data.pokemon.length; i++) {     
         contenedor += 
-        `<div class="card">
+        `<div class="card" type = "button">
             <div class= "border-small">
                 <h2 class="poke-Number">N.° ${data.pokemon[i].num}</h2>
                 <div class="clasifics">
@@ -40,7 +53,7 @@ function AppendData(data){
                 <p class="poke-name"> ${Capital(data.pokemon[i].name)} </p> 
                 <p class="description"> ${data.pokemon[i].about} </p>
             </div>
-        </div>`
+        </div>`    
     }
     container.innerHTML = contenedor
 }
